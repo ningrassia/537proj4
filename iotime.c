4679bdfb4ad0f426955b1062feda2526fac98a0c
Nik Ingrassia <ningrassia@wisc.edu>
2013-11-26 15:12:40 -0600

    Fixed things. Also, made write/fwrite both use for loops.

--- iotime.c
+++ iotime.c
@@ -6,8 +6,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-
-#define 128_MEG 134217728
+#include <sys/stat.h>
+#include <fcntl.h>
+#define ONETWOEIGHTMEG 134217728
 
 int main(int argc, char** argv)
 {
@@ -23,18 +24,16 @@ int main(int argc, char** argv)
 	/*file descriptor for dump file*/
 	int dump_fd;
 
-	/*structs to hold time data for each run of write/frwite*/
+	/*structs to hold time data for each run of write/frwite, and the elapsed time*/
 	struct timeval * before_time;
 	struct timeval * after_time;
+	struct timeval * elapsed_time;
 
 	/*hold the current size we're using to write to disk*/
 	int write_size;
 
 	/*hold number of things of the size to write to the disk*/
 	int num_write;
-
-	/*number of times to call write*/
-	int write_loop;
 
 	/*String to hold file names! reusable!*/
 	char * filename;
@@ -62,16 +61,16 @@ int main(int argc, char** argv)
 	int i;
 
 	/*We need to specify an input and output file!*/
-	if(argc != 2)
+	if(argc != 3)
 	{
 		printf("Please specify an input file and an output file.\n");
 		exit(EXIT_FAILURE);
 	}
 
 	/*Try to open up the input file*/
-	filename = malloc(strlen(argv[0]) + strlen(relpath) + 1);
+	filename = malloc(strlen(argv[1]) + strlen(relpath) + 1);
 	filename = strncat(filename, relpath, strlen(relpath));
-	filename = strncat(filename, argv[0], strlen(argv[0]));
+	filename = strncat(filename, argv[1], strlen(argv[1]));
 	input = fopen(filename, "r");
 	if(input == NULL)
 	{
@@ -81,10 +80,10 @@ int main(int argc, char** argv)
 	free(filename);
 
 	/*Open up the output file*/
-	filename = malloc(strlen(argv[1]) + strlen(relpath) + 1);
-	filename = strncat(filename, argv[1], strlen(argv[1]));
+	filename = malloc(strlen(argv[2]) + strlen(relpath) + 1);
 	filename = strncat(filename, relpath, strlen(relpath));
-	output = fopen(argv[1], "w");
+	filename = strncat(filename, argv[2], strlen(argv[2]));
+	output = fopen(filename, "w");
 	if(output == NULL)
 	{
 		perror("Error on opening/creating output file - ");
@@ -95,9 +94,10 @@ int main(int argc, char** argv)
 	/*initialize our time structs*/
 	before_time = malloc(sizeof(struct timeval));
 	after_time = malloc(sizeof(struct timeval));
+	elapsed_time = malloc(sizeof(struct timeval));
 
 	/*make a big huge buffer to write from*/
-	write_buffer = malloc(128_MEG);
+	write_buffer = malloc(ONETWOEIGHTMEG);
 
 	/*
 	 * Here's the meat of the program.
@@ -114,20 +114,21 @@ int main(int argc, char** argv)
 
 		/*Read in the size to write!*/
 		fscanf_return = fscanf(input, "%i", &write_size);
-		fprintf(output, "Doing 10 writes of size %i", write_size);
+		printf("Doing 10 writes of size %i\n", write_size);
 		/*calculate the number of times to do the write*/
-		num_write = 128_MEG/write_size;
+		num_write = ONETWOEIGHTMEG/write_size;
 		for(count = 0; count < 10; count++)
 		{
 			/*Here we use write*/
+			printf("write %d of size %d\n", count, write_size);
 			gettimeofday_return = gettimeofday(before_time, NULL);
 			if(gettimeofday_return == -1)
 			{
 				perror("Error on gettimeofday - ");
 				exit(EXIT_FAILURE);
 			}
-			/*open the file*/
-			dump_fd = open(tempnam("/tmp/", "dump"));
+			/*open the file and make sure to make it a clean file!*/
+			dump_fd = open(tempnam("/tmp/", "dump"), O_WRONLY | O_CREAT | O_TRUNC | O_EXCL);
 			if(dump_fd < 0)
 			{
 				perror("Error on open - ");
@@ -135,7 +136,7 @@ int main(int argc, char** argv)
 
 			}
 			/*write to the file with a size of the buffer write_loop times!)*/
-			for(i = 0; i < num_write, i++)
+			for(i = 0; i < num_write; i++)
 			{
 				write_return = write(dump_fd, write_buffer, write_size);
 				if(write_return < 0)
@@ -149,8 +150,7 @@ int main(int argc, char** argv)
 			if(close_return < 0)
 			{
 				perror("Error on close - ");
-				exit(EXIT_		write_buffer = realloc(write_buffer, write_size);
-FAILURE);
+				exit(EXIT_FAILURE);
 			}
 			gettimeofday_return = gettimeofday(after_time, NULL);
 			if(gettimeofday_return == -1)
@@ -158,8 +158,10 @@ FAILURE);
 				perror("Error on gettimeofday - ");
 				exit(EXIT_FAILURE);
 			}
-			fprintf(output, "write %d: From %d seconds %d microseconds to %d seconds %d microseconds with %d calls to write\n", count, (int)before_time->tv_sec, (int)before_time->tv_usec, (int)after_time->tv_sec, (int)after_time->tv_usec, num_write);
+			timersub(after_time, before_time, elapsed_time);
+			fprintf(output, "write %d: %d seconds %d microseconds with %d calls to write\n", count, (int)elapsed_time->tv_sec, (int)elapsed_time->tv_usec, num_write);
 			/*And here we use fwrite*/
+			printf("fwrite %d of size %d\n", count, write_size);
 			gettimeofday_return = gettimeofday(before_time, NULL);
 			if(gettimeofday_return == -1)
 			{
@@ -167,14 +169,16 @@ FAILURE);
 				exit(EXIT_FAILURE);
 			}
 			/*open the file*/
-			dump = fopen(tempnam("/tmp/", "dump"));
+			dump = fopen(tempnam("/tmp/", "dump"), "w");
 			if(dump == NULL)
 			{
 				perror("Error on fopen - ");
 				exit(EXIT_FAILURE);
 			}
 			/*write to the file*/
-			fwrite(write_buffer, write_size, num_write, dump);
+			for(i = 0, i < num_write, i++){
+				fwrite(write_buffer, write_size, 1, dump);
+			}
 			if(ferror(dump))
 			{
 				perror("Error on fwrite - ");
@@ -193,11 +197,18 @@ FAILURE);
 				perror("Error on gettimeofday - ");
 				exit(EXIT_FAILURE);
 			}
-			fprintf(output, "fwrite %d: From %d seconds %d microseconds to %d seconds %d microseconds with 1 call to fwrite\n", count, (int)before_time->tv_sec, (int)before_time->tv_usec, (int)after_time->tv_sec, (int)after_time->tv_usec);
+			/*find the elapsed time! yay macros*/
+			timersub(after_time, before_time, elapsed_time);
+			fprintf(output, "fwrite %d: %d seconds %d microseconds with 1 call to fwrite\n", count, (int)elapsed_time->tv_sec, (int)elapsed_time->tv_usec);
+
+			/*write to our output file after each run just to be safe/lazy*/
+			fflush(output);
 
 		}
 	}
 	/*clean up our toys when we're done*/
+	fclose(input);
+	fclose(output);
 	free(write_buffer);
 	free(after_time);
 	free(before_time);
